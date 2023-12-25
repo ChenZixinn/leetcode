9dcbbf9fd22b5aaa19f5a129bd43f96183004e16
tzuxinchen <15014324839@163.com>
2023-12-25 14:35:17 +0800

    层序遍历

--- common/data_structure.py
+++ common/data_structure.py
@@ -1,5 +1,3 @@
-
-
 class TreeNode:
 
     def __init__(self, val=0, left=None, right=None):
@@ -11,4 +9,12 @@ class TreeNode:
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
+        self.next = next
+
+
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
         self.next = next
