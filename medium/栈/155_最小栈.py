c26c2573c8da60e300bc1c4b395568c5a8f1a9bf
tzuxinchen <15014324839@163.com>
2024-01-01 21:55:29 +0800

    面试题73/150

---
+++ medium/栈/155_最小栈.py
@@ -0,0 +1,48 @@
+"""
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+实现 MinStack 类:
+
+MinStack() 初始化堆栈对象。
+void push(int val) 将元素val推入堆栈。
+void pop() 删除堆栈顶部的元素。
+int top() 获取堆栈顶部的元素。
+int getMin() 获取堆栈中的最小元素。
+"""
+import math
+
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = [int(math.inf)]
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.min_stack.append(min(val, self.min_stack[-1]))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+# Your MinStack object will be instantiated and called as such:
+obj = MinStack()
+obj.push(1)
+obj.push(2)
+obj.pop()
+param_3 = obj.top()
+param_4 = obj.getMin()
